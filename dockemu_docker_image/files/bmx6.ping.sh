64a03aedc41a550750695e855b2005075b3354a2
Marcos Cano <mcano@stsa.info>
2015-10-12 21:16:41 -0600

    Mon Oct 12 21:16:40 CST 2015

--- dockemu_docker_image/files/bmx6.ping.sh
+++ dockemu_docker_image/files/bmx6.ping.sh
@@ -2,6 +2,7 @@
 count=5
 every=5
 while : ;do  
+	echo "** MY IP: **" $(ifconfig |grep "inet6 addr:" |grep "Scope:Global"|awk '{print $3}')
 	
 	#bmx6 connect  show=originators  |awk '{print $3}'|xargs -i ping6 -c $count {}
 	bmx6 connect  show=originators |awk '$4 != 0 { print $3 }'|xargs -i ping6 -c $count {}
--- dockemu_docker_image/files/general.ping.sh
+++ dockemu_docker_image/files/general.ping.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-route -A inet6 |grep eth0 |grep UG |awk '{print $1}' |tr "/" " "|awk '{print $1}'|xargs -i ping6  -c 4 {} 
+count=5
+every=5
+while : ;do  
+	echo "** MY IP: **" $(ifconfig |grep "inet6 addr:" |grep "Scope:Global"|awk '{print $3}')
+	route -A inet6 |grep eth0 |grep UG |awk '{print $1}' |tr "/" " "|awk '{print $1}'|xargs -i ping6  -c 4 {} 
+	sleep $every
+done
